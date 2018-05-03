1856760807db6e8f6347f621192cf5f5c2a075c8
Mark Reynolds <mark.reynolds@digital.hmrc.gov.uk>
2018-05-03 11:58:27 +0100

    DDCNLS-3020: Roll back nonukpostcode verifier

--- app/utils/GovernmentGatewayConstants.scala
+++ app/utils/GovernmentGatewayConstants.scala
@@ -24,6 +24,6 @@ object GovernmentGatewayConstants {
   val FRIENDLY_NAME = "ATED Enrolment"
 
   val VerifierPostalCode = "Postcode"
-  val VerifierNonUKPostalCode = "PostCode"
+  val VerifierNonUKPostalCode = "NonUKPostalCode"
   val VerifierCtUtr= "CTUTR"
 }
