fb4c0c8a0c2cea2d0ab86d933677055a44e6dbd3
Ufuoma Daniel <29196699+Ufuoma-Daniel@users.noreply.github.com>
2019-10-22 13:28:14 +0100

    Reintroduced HeaderCarrierPartialsConverter

--- app/connectors/AgentClientMandateFrontendConnector.scala
+++ app/connectors/AgentClientMandateFrontendConnector.scala
@@ -22,12 +22,13 @@ import models.{AgentEmail, ClientDisplayName, OldMandateReference}
 import play.api.mvc.Request
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.http.DefaultHttpClient
+import uk.gov.hmrc.play.partials.HeaderCarrierForPartialsConverter
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class AgentClientMandateFrontendConnector @Inject()(appConfig: ApplicationConfig,
                                                     http: DefaultHttpClient
-                                                   ) extends RawResponseReads {
+                                                   ) extends RawResponseReads with HeaderCarrierForPartialsConverter {
 
   val serviceUrl: String = appConfig.serviceUrlACMFrontend
   val emailUri = "mandate/agent/email-session"
@@ -35,18 +36,19 @@ class AgentClientMandateFrontendConnector @Inject()(appConfig: ApplicationConfig
   val mandateDetails = "mandate/agent/old-nonuk-mandate-from-session"
   val service = "ATED"
 
+  override def crypto: String => String = identity
 
-  def getAgentEmail(implicit request: Request[_], hc: HeaderCarrier, ec: ExecutionContext): Future[Option[AgentEmail]] = {
+  def getAgentEmail(implicit request: Request[_], ec: ExecutionContext): Future[Option[AgentEmail]] = {
     val getUrl = s"$serviceUrl/$emailUri/"
     http.GET[Option[AgentEmail]](getUrl)
   }
 
-  def getClientDisplayName(implicit request: Request[_], hc: HeaderCarrier, ec: ExecutionContext): Future[Option[ClientDisplayName]] = {
+  def getClientDisplayName(implicit request: Request[_], ec: ExecutionContext): Future[Option[ClientDisplayName]] = {
     val getUrl = s"$serviceUrl/$displayNameUri/"
     http.GET[Option[ClientDisplayName]](getUrl)
   }
 
-  def getOldMandateDetails(implicit request: Request[_], hc: HeaderCarrier, ec: ExecutionContext): Future[Option[OldMandateReference]] = {
+  def getOldMandateDetails(implicit request: Request[_], ec: ExecutionContext): Future[Option[OldMandateReference]] = {
     val getUrl = s"$serviceUrl/$mandateDetails/"
     http.GET(getUrl) map { response => response.json.asOpt[OldMandateReference]}
   }
