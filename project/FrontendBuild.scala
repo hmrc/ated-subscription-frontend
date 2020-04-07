d48cad0aca6f552c55106580b0f389f4c8facc8a
davidmthornton <28890110+davidmthornton@users.noreply.github.com>
2020-04-07 15:30:44 +0100

    Dependency updates

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -15,8 +15,8 @@ private object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.48.0-play-26",
-    "uk.gov.hmrc" %% "play-ui" % "8.7.0-play-26",
+    "uk.gov.hmrc" %% "govuk-template" % "5.54.0-play-26",
+    "uk.gov.hmrc" %% "play-ui" % "8.8.0-play-26",
     "uk.gov.hmrc" %% "play-partials" % "6.9.0-play-26",
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "9.0.0-play-26",
@@ -33,11 +33,11 @@ private object AppDependencies {
       override lazy val test = Seq(
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3" % scope,
         "org.pegdown" % "pegdown" % "1.6.0",
-        "org.jsoup" % "jsoup" % "1.12.1" % scope,
+        "org.jsoup" % "jsoup" % "1.13.1" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
         "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-26",
         "org.mockito" % "mockito-core" % "3.2.4" % scope,
-        "com.github.tomakehurst" % "wiremock-jre8" % "2.21.0" % "test,it"
+        "com.github.tomakehurst" % "wiremock-jre8" % "2.26.3" % "test,it"
       )
     }.test
   }
