0dc54e77af173f20af849e2cc0eb4ece134622d5
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2020-04-08 10:05:42 +0100

    DL-2890 further dependency and plugin updates

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -14,13 +14,13 @@ private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.7.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.54.0-play-26",
     "uk.gov.hmrc" %% "play-ui" % "8.8.0-play-26",
-    "uk.gov.hmrc" %% "play-partials" % "6.9.0-play-26",
+    "uk.gov.hmrc" %% "play-partials" % "6.10.0-play-26",
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "9.0.0-play-26",
-    "uk.gov.hmrc" %% "auth-client" % "2.32.2-play-26"
+    "uk.gov.hmrc" %% "auth-client" % "2.35.0-play-26"
   )
 
   trait TestDependencies {
