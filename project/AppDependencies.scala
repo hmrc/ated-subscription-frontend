4985a7960bd95f67e7c47c27c54b3ae02f86968a
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2021-12-03 10:30:32 +0000

    DL-6850 apply missing test config in dependencies plus upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ private object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.16.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "1.26.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "1.27.0-play-28",
     "uk.gov.hmrc" %% "play-partials"              % "8.2.0-play-28",
     "uk.gov.hmrc" %% "domain"                     % "6.2.0-play-28",
     "uk.gov.hmrc" %% "http-caching-client"        % "9.5.0-play-28"
@@ -22,15 +22,15 @@ private object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "5.16.0",
+        "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "5.16.0"            % scope,
         "org.scalatestplus.play"       %% "scalatestplus-play"     % "5.1.0"             % scope,
-        "org.pegdown"                  %  "pegdown"                % "1.6.0",
+        "org.pegdown"                  %  "pegdown"                % "1.6.0"             % scope,
         "org.jsoup"                    %  "jsoup"                  % "1.14.3"            % scope,
         "com.typesafe.play"            %% "play-test"              % PlayVersion.current % scope,
         "org.scalatestplus"            %% "mockito-3-12"           % "3.2.10.0"          % scope,
         "org.mockito"                  %  "mockito-core"           % "4.1.0"             % scope,
         "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.0"            % scope,
-        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.31.0"            % scope
+        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.32.0"            % scope
       )
     }.test
   }
