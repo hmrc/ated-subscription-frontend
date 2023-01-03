59cfc457072ec178853a193a4705b9baf23d6613
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-01-03 11:41:27 +0000

    DL-7172 added panel and button components to more views

--- test/controllers/AgentConfirmationControllerSpec.scala
+++ test/controllers/AgentConfirmationControllerSpec.scala
@@ -76,7 +76,7 @@ class AgentConfirmationControllerSpec extends PlaySpec with GuiceOneServerPerSui
 
             val document = Jsoup.parse(contentAsString(result))
             document.title() must be("You have successfully set up this agency for ATED - GOV.UK")
-            document.getElementById("agent-reference").text() must be(s"You have successfully set up ACME LTD for ATED on ${Dates.formatDate(LocalDate.now())}")
+            document.getElementById("banner").text() must be(s"You have successfully set up ACME LTD for ATED on ${Dates.formatDate(LocalDate.now())}")
             document.getElementById("submit").text() must be("Add my ATED clients")
           }
         }
