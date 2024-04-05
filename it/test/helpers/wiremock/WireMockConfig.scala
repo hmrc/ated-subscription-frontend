625a9cbf60a73c0ed4425d601de03c4593028ede
rameshmandanapu1 <155470931+rameshmandanapu1@users.noreply.github.com>
2024-04-05 08:29:29 +0100

    akka and few other minor tweaks

---
+++ it/test/helpers/wiremock/WireMockConfig.scala
@@ -0,0 +1,7 @@
+package helpers.wiremock
+
+trait WireMockConfig {
+  val wireMockPort: Int    = 11111
+  val wireMockHost: String = "localhost"
+  val wireMockUrl: String  = s"http://$wireMockPort:$wireMockPort"
+}
