bc82e8767ff1f15aa910ea4e83e6f9bea66d7ab1
nadinJ <63252254+nadinJ@users.noreply.github.com>
2024-06-14 12:21:11 +0100

    DL-13829 play-frontend-hmrc upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ private object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"  % "8.5.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"  % "8.5.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"  % "10.1.0",
     "uk.gov.hmrc"   %% "play-partials-play-30"       % "9.1.0",
     "uk.gov.hmrc"   %% "domain-play-30"              % "9.0.0",
     "uk.gov.hmrc"   %% "http-caching-client-play-30" % "11.2.0",
