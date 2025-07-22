cae7deed76adf76d2e2e857fcd21ef011d34d297
preetampotdar <10691333+preetampotdar@users.noreply.github.com>
2025-07-22 11:54:36 +0100

    feat(DL-16831): Enabled brand refresh changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,11 @@ import sbt.*
 
 private object AppDependencies {
 
+  val bootstrapVersion = "9.18.0"
+
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"  % "9.13.0",
+    "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"  % "12.7.0",
     "uk.gov.hmrc"   %% "play-partials-play-30"       % "10.1.0",
     "uk.gov.hmrc"   %% "domain-play-30"              % "11.0.0",
@@ -15,7 +17,7 @@ private object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-test-play-30"% "9.13.0",
+    "uk.gov.hmrc"   %% "bootstrap-test-play-30"% bootstrapVersion,
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = Seq()
