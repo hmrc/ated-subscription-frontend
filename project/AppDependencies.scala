21d7612fcb880d47c208844952ae240b8d39c96e
vierlan <28763492+vierlan@users.noreply.github.com>
2025-11-11 16:02:02 +0000

    DL-17748: Add runAllChecks to ATED / ACM repos

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,16 +4,16 @@ import sbt.*
 
 private object AppDependencies {
 
-  val bootstrapVersion = "9.19.0"
+  val bootstrapVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"  % "12.8.0",
-    "uk.gov.hmrc"   %% "play-partials-play-30"       % "10.1.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"  % "12.20.0",
+    "uk.gov.hmrc"   %% "play-partials-play-30"       % "10.2.0",
     "uk.gov.hmrc"   %% "domain-play-30"              % "11.0.0",
     "uk.gov.hmrc"   %% "http-caching-client-play-30" % "12.2.0",
-    "commons-codec" %  "commons-codec"               % "1.18.0",
+    "commons-codec" %  "commons-codec"               % "1.20.0",
   )
 
   val test: Seq[ModuleID] = Seq(
