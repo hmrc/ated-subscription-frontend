cb2d3ffdacab466a72ee87e13194b14d7796228f
nehakannaujia1 <50657531+nehakannaujia1@users.noreply.github.com>
2026-07-22 19:20:41 +0100

    removed print

--- app/forms/AtedForms.scala
+++ app/forms/AtedForms.scala
@@ -183,7 +183,6 @@ object AtedForms {
   def verifyUKPostCode(correspondenceForm: Form[Address]): Form[Address] = {
     if (!correspondenceForm.hasErrors) {
       val country = correspondenceForm.data.get("country")
-      println("*****************************************" + country)
       val errors = country match {
         case Some("GB") =>
           correspondenceForm.data.get("postcode") match {
